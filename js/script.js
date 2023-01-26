eb5d5b8b8164051b550be3909b11e7ce92eb92c4
antelor <ramiro.antelo4@gmail.com>
2023-01-26 19:42:05 -0300

    dom logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-function getComputerChoice(){
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
 
     let choice = Math.floor(Math.random () * 3);
@@ -6,8 +9,8 @@ function getComputerChoice(){
     return choices[choice];
 }
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection){
+    let computerSelection = getComputerChoice()
 
     if (playerSelection === computerSelection){
         return 'Empate';
@@ -16,10 +19,12 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection === 'rock'){
         switch (computerSelection){
             case 'paper':
+            computerScore++;
             return 'pc';
             break;
 
             case 'scissors':
+            playerScore++;
             return 'user';
             break;
         }
@@ -27,10 +32,12 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection === 'paper'){
         switch (computerSelection){
             case 'rock':
+            playerScore++;
             return 'user';
             break;
 
             case 'scissors':
+            computerScore++;
             return 'pc';
             break;
         }
@@ -38,26 +45,44 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection === 'scissors'){
         switch (computerSelection){
             case 'paper':
+            playerScore++;
             return 'user';
             break;
 
             case 'rock':
+            computerScore++;
             return 'pc';
             break;
         }
     }
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for(let i = 0; i < 5; i++){
-        let ronda = playRound(getComputerChoice(), getComputerChoice());
-        (ronda === 'user' ? playerScore++ : computerScore++);  
-    }
+//button listeners
+let buttons = document.querySelectorAll(".selection");
+let results = document.querySelector(".results");
+let computerScoreElement = document.querySelector("#computerScore");
+let playerScoreElement = document.querySelector("#playerScore");
 
-    return (playerScore>computerScore ? 'Gana usuario' : 'Gana PC');
-}
+buttons.forEach((button) => {  
+  button.addEventListener('click', () => {
+      let p = document.createElement('p');
+      p.textContent = playRound(button.id);
+      results.appendChild(p);
 
-console.log(game());
+      computerScoreElement.textContent = computerScore;
+      playerScoreElement.textContent = playerScore;
+
+      if (computerScore === 5) {
+        let p2 = document.createElement('h1');
+        p2.textContent = "gana pc";
+        results.appendChild(p2);
+      }
+
+
+      if (playerScore === 5) {
+        let p2 = document.createElement('h1');
+        p2.textContent = "gana player";
+        results.appendChild(p2);
+      }
+  });
+});
